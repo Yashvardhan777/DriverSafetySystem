b982799a0ef1ddb5f35b807eb996d234e75d7b38
Yashvardhan777 <57723640+Yashvardhan777@users.noreply.github.com>
2021-06-23 13:52:09 +0530

    Changed UI

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -9,7 +9,7 @@ import time
 face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
 leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
 reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
-model = load_model('models/cnncat2.h5')
+model = load_model('models/DDS.h5')
 path = os.getcwd()
 
 
